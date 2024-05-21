1028aec12333072c15eafb7cb909918f10cf27d5
halkhattab63 <100689695+halkhattab63@users.noreply.github.com>
2024-05-21 20:43:43 +0300

    2.commit

---
+++ src/A.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HAYYAN ALKHATTAB
+ */
+public class A {
+    public static void main(String[] args)
+    {
+        System.out.println("A");
+    }
+}
+
+class B {
+    public static void main(String[] args)
+    {
+        System.out.println("B");
+    }
+}
+
+class C {
+    public static void main(String[] args)
+    {
+        System.out.println("C");
+    }
+
+}
+
+class D {
+    
+}
